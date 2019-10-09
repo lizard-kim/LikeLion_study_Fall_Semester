4ebb803dabdbdb0714d3745fe431691d9a792db4
taeyeong <rlaxodud1122@gmail.com>
2019-10-09 16:11:01 +0900

    2week add annotations

--- 2week/firstrest/post/serializer.py
+++ 2week/firstrest/post/serializer.py
@@ -1,9 +1,9 @@
 from .models import Post
-from rest_framework import serializers
+from rest_framework import serializers # use rest_framework
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__' # make all field serializer
         # fields = ['title', 'body'] make title and body fields serializer 
-        read_only_fields = ('title', ) # tuple recommended
+        read_only_fields = ('title', ) # tuple recommended / title을 read_only로 만들수 있다.
--- 2week/firstrest/post/views.py
+++ 2week/firstrest/post/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 from rest_framework import viewsets # what is viewsets?
 from .models import Post
 from .serializer import PostSerializer
@@ -7,5 +5,5 @@ from .serializer import PostSerializer
 #CBV
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    queryset = Post.objects.all() # Post model의 모든 것을 가져다 queryset으로 쓰겠다.
+    serializer_class = PostSerializer # PostSerializer를 사용하겠다.
